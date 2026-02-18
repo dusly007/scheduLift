0ae6b697d903a5278f64cdb94cb7ad3544926514
Jonathan Valadez-Riquelme <6176721@cegepmv.ca>
2026-02-18 16:34:33 -0500

    Message de votre commit

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { UsersService } from "./users.service";
 
 @Controller('users')
 export class UsersController {
+    
+        constructor(private usersService: UsersService) {}
 
+        @Post()
+        create(@Body() body : any) {
+            console.log(body);
+        }
 }
