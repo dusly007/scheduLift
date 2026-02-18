fe00b35fd20c9d75b4044aa41a31b5573bd76851
Dusly Nestor <2395223@cegepmv.ca>
2026-02-18 16:14:52 -0500

    User

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
 
