609ff332cc42a89a941411c6c79b9436d1724a74
Jonathan Valadez-Riquelme <6176721@cegepmv.ca>
2026-04-08 15:29:33 -0400

    Implémenter update dto, courses service et courses controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -3,33 +3,37 @@ import { CoursesService } from "./courses.service";
 import { CreateCourseDto } from "./dtos/create-course.dto";
 import { CurrentUser } from "src/auth/decorators/current-user.decorator";
 import { User } from "src/users/user.entity";
+import { UpdateCoursesDto } from "./dtos/update-courses.dto";
+import { AuthGuard } from "src/auth/guards/auth.guards";
+import { CoachGuard } from "src/users/guards/coach.guard";
+import { AdminGuard } from "src/users/guards/admin.guards";
 
 @Controller('courses')
 export class CoursesController {
 
     constructor(private coursesService: CoursesService) {}
     //faire les routes des autres méthode
-
+    @UseGuards(CoachGuard)
     @Post()
     create(@Body() body: CreateCourseDto, @CurrentUser() user : User) {
       return this.coursesService.createCourse(body, user);
     }
-
+   @UseGuards(AuthGuard)
     @Get()
     findAllCourses() {
       return this.coursesService.findAllCourses();
     }
-  
+    @UseGuards(AuthGuard)
     @Get('/:id')
     findCourseById(@Param('id') id: string) {
-      return this.coursesService.findCourseById(parseInt(id));
+      return this.coursesService.findCourseById(parseInt(id)); 
     }
-  
+    @UseGuards(CoachGuard)
     @Patch('/:id')
-    updateCourse(@Param('id') id: string, @Body() body: Partial<CreateCourseDto>) {
+    updateCourse(@Param('id') id: string, @Body() body: Partial<UpdateCoursesDto>) {
       return this.coursesService.updateCourse(parseInt(id), body);
     }
-  
+    @UseGuards(CoachGuard)
     @Delete('/:id')
     deleteCourse(@Param('id') id: string) {
       return this.coursesService.deleteCourse(parseInt(id));
